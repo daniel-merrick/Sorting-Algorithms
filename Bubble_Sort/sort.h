e7fe41d5a56a8b6c6ef1870020b42a25e822d69c
Daniel Paul Merrick <dmerrick@ecegrid-thin1.ecn.purdue.edu>
2018-03-13 18:46:18 -0400

    Bubble Sort

---
+++ Bubble_Sort/sort.h
@@ -0,0 +1,36 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*****************************
+ * store number of long ints
+ * from input file
+ * in 'Size' and return a long
+ * *array of stored elements
+ * read from input file
+ * return NULL on failure
+*****************************/
+long *Read_From_File(char *Filename, long *Size);
+
+/*****************************
+ * Save array of long ints
+ * to outputFile and return 
+ * false on failure
+*****************************/
+bool Save_To_File(char *Filename, long *Elements_, long Size);
+
+
+/******************************
+ * Perform SelectionSort on
+ * array of Elements_
+******************************/
+void BubbleSort(long *Elements_, long Size);
+
+
+/*****************************
+ * Check if array of Elements_
+ * is sorted ...
+ * return false if not sorted
+*****************************/
+bool CheckSort(long *Elements_, long Size_);
